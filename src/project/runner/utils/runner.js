d6c067638bbfe734817c334aeac4f6fe37d69f3e
wuzhidewuchi@gmail.com <wuzhidewuchi@gmail.com>
2017-11-13 22:59:51 +0800

    add runner index page

--- src/project/runner/utils/runner.js
+++ src/project/runner/utils/runner.js
@@ -1,8 +1,9 @@
 export default class Runner {
-    constructor(documentId, runway, limitTime, maxV, person, theEndShape, winW, winH, total, runV, shapes, animationV, downV) {
+    constructor(documentId, runway, bg, limitTime, maxV, person, theEndShape, winW, winH, total, runV, shapes, animationV, downV) {
         this.runway = runway
         this.maxV = maxV
         this.winW = winW
+        this.bg = bg
         this.winH = winH
         this.total = total
         this.downV = downV
@@ -15,6 +16,7 @@ export default class Runner {
         this.isPlay = false
         this.showTime = false
         this.showReady = false
+        this.showButton = false
         this.runMetres = 0
         this.ready123 = 1
         this.canvas = document.getElementById(documentId)
@@ -34,11 +36,6 @@ export default class Runner {
             }, 166.66 / this.animationV)
         }
     }
-
-    levelUp() {
-
-    }
-
     start() {
         this.shapes[2] = this.person
         this.isPlay = true
@@ -77,49 +74,6 @@ export default class Runner {
         this.person.distance = this.runMetres
         clearTimeout(this.limitTimeInterVal)
         clearTimeout(this.downVSetTime)
-        // if (this.data.sender) {
-        //     this.total = parseInt(this.data.total) + parseInt(this.runMetres)
-        // }
-        // else {
-        //     this.total = parseInt(this.runMetres) > parseInt(this.data.total) ? parseInt(this.runMetres) : parseInt(this.data.total)
-        // }
-        // var prize = prizelist[this.level - 1]
-        // var surplusM = parseInt(prize.distanc) - parseInt(this.total)
-        // var level = this.data.sender ? this.data.sender.level : 1
-        //
-        // if (surplusM <= 0) {
-        //     this.level = parseInt(parseInt(this.total) / 100) + 1
-        //     prize = this.level > 13 ? prizelist[12] : prizelist[this.level - 2]
-        //     surplusM = parseInt(prize.distanc) - parseInt(this.total)
-        //     //if(prize.level>=4){
-        //     //    $(".surprise").html("哇喔！魔鬼的指法！");
-        //     //    $(".prize-img").attr("src","images/prize2.jpg")
-        //     //}
-        //     var prize_msg = '恭喜你,到达<span class="city"></span>'
-        //     if (this.sender) {
-        //         prize_msg = '恭喜你帮<span class="name">name</span>到达<span class="city"></span>'
-        //     }
-        //     $('.prize-msg').html(prize_msg)
-        //     $('.prizelist').show()
-        //     this.levelUp = true
-        // }
-        // else {
-        //     if (this.data.friends.length == 0) {
-        //         $('.surplusM').html(prize.distanc)
-        //     }
-        // }
-        // this.level > 13 ? $('.levelCount').html(parseInt(14 - level)) : $('.levelCount').html(parseInt(this.level - level))
-        // $('.city').html(prize.city)
-        // $('.prize-desc').html(prize.desc)
-        // $('.surplusM').html(surplusM)
-        //
-        // $('.game .title .time').addClass('move')
-        // $('.game .title .result').addClass('moveL')
-        // $('#myMetres').html(this.total)
-        // $('#runway li:first-child .head-photo p').html(this.runMetres + 'm')
-        // 添加用户
-        // addUser()
-        document.getElementById('audioShaking').pause()
     }
 
     ready() {
@@ -152,38 +106,13 @@ export default class Runner {
     init() {
         this.winW = window.innerWidth
         this.winH = window.innerHeight
-        this.imgAddress = ['images/persons.png', 'images/tree.png', 'images/ready.png', 'images/game-bg.jpg', 'images/theEnd.png', 'images/msg-bg.png', 'images/prizelist-bg.jpg']
-        person = new Shap((this.winW - 125) / 2, (this.winH - 125) / 2, 111, 275, 1, this.imgAddress[0], 0, 0, 162, 400, 162, p_movement)
-        runway = new Shap(0, 0, this.winW, this.winH, 1, this.imgAddress[1], 0, 0, 640, 960, 640, runway_movement)
-        bg = new Shap(0, 0, this.winW, this.winH, 1, this.imgAddress[3], 0, 0, 640, 960, 0, p_movement)
-        this.ready = new Shap((this.winW - 175) / 2, (this.winH - 75) / 2, 175, 323, 1, this.imgAddress[2], 0, 0, 246, 455, 0, p_movement)
-        this.theEnd = new Shap((this.winW - 175) / 2, (this.winH - 75) / 2, 175, 356, 1, this.imgAddress[4], 0, 0, 279, 568, 0, p_movement)
-        cxt.fillStyle = 'rgb(0,0,0)'
-        this.time_15 = 100
-        this.animate_v = 2
-        this.runV = 10
-        this.maxV = 8
-        this.ready123 = 1
-        this.shaps.push(bg)
-        this.shaps.push(runway)
-        this.shaps.push(this.ready)
-        canvas.attr('width', this.winW)
-        canvas.attr('height', this.winH)
-        $('#button i').show()
-        $('#ready').show()
-        $('.game .title .result').removeClass('moveL')
-        $('.game .title .time').removeClass('move')
-        $('.game .title .time').hide()
-        $('#metres').html('0')
-        $('#time').html('15.0s')
-        $('.runway-body').hide()
-        $('.game').show()
-        startButton.show()
-        //加载页面，加载运动图片加载完毕执行下一步
-        loading(function () {
-            $('.loading').fadeOut(300)
-            clearTimeout(this.setLoadTime)
-            animate(true, cxt)
-        }, this.imgAddress)
+        this.canvas.attr('width', this.winW)
+        this.canvas.attr('height', this.winH)
+        this.cxt.fillStyle = 'rgb(0,0,0)'
+        this.shapes.push(this.bg)
+        this.shapes.push(this.runway)
+        this.shapes.push(this.ready)
+        this.showReady = true
+        this.showButton = true
     }
 }
--- src/project/runner/views/home/index.vue
+++ src/project/runner/views/home/index.vue
@@ -1,18 +1,291 @@
 <template lang="pug">
-    .runner-container 啦啦啦奔跑
+    .runner-container
+        .game
+            .game-body
+            .title
+                .ready(id="ready")
+                .ready123(id="ready123")
+                    p.go123.go_1
+                .time
+                h2(id="time")
+                    b 15.0s
+                p.metres 腻害，跑了
+                    span(id="metres") 84
+                    | Km
+                .result
+                    p.name.myName name
+                    p 腻害！跑了
+                        span.levelCount 城市
+            canvas(id="myCanvas" width="1000" height="1000")
+            .button(id="button")
+                section
+                    p
+                        span.finger-l(:class='{active:showFingerL}',@click="touchHandle")
+                        i
+                    p
+                        span.finger-r(:class='{active:showFingerR}',@click="touchHandle")
+                        i
 
 
 </template>
 <style lang="less" rel="stylesheet/less" scoped>
     @import "../../../../assets/less/fn.less";
 
+    .button {
+        position: absolute;
+        width: 100%;
+        bottom: 0;
+    }
+    .button section{
+        width: 100%;
+        font-size: 5px;
+        text-align: center;
+    }
+    .button section p{
+        float: left;
+        width: 50%;
+        text-align: center;
+        padding: 20px 0;
+    }
+    .button section .finger-l{
+        background: url("/game/static/runner/fingerprint.jpg") center center no-repeat #cfcfcf;
+        width: 90%;
+        background-size: 40px auto;
+        height: 58px;
+        display: inline-block;
+        border-radius: 5px;
+        margin-right: 10px;
+        position: relative;
+    }
+    .button section .finger-l.active i{
+        background: url("/game/static/runner/icon-finger-l.png") no-repeat;
+        width: 90%;
+        height: 98px;
+        background-size: 150px auto;
+        position: absolute;
+        left: 50%;
+        top:-17px;
+        margin-left: -75px;
+    }
+    .button section .finger-l.active{
+        background: url("/game/static/runner/fingerprint_act-r.jpg") center center no-repeat #f0ff00;
+        background-size: 40px auto;
+    }
+
+
+    .button section .finger-r{
+        background: url("/game/static/runner/fingerprint-r.jpg") center center no-repeat #cfcfcf;
+        width: 90%;
+        background-size: 40px auto;
+        height: 58px;
+        display: inline-block;
+        border-radius: 5px;
+        margin-right: 10px;
+        position: relative;
+    }
+    .button section .finger-r.active i{
+        background: url("/game/static/runner/icon-finger-r.png") no-repeat;
+        width: 90%;
+        height: 98px;
+        background-size: 150px auto;
+        position: absolute;
+        left: 50%;
+        top:-17px;
+        margin-left: -75px;
+    }
+    .button section .finger-r.active{
+        background: url("/game/static/runner/fingerprint_active.jpg") center center no-repeat #f0ff00;
+        background-size: 40px auto;
+    }
+
+
+    /*loading*/
+    .loading {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background: #fff;
+        z-index: 99999;
+        background: url("/game/static/runner/loading.gif") no-repeat center center;
+        background-size: cover;
+    }
+
+    .loading .l-bg, .loading .l-bar {
+        width: 90%;
+        height: 3px;
+        background: #22252b;
+        position: absolute;
+        top: 54%;
+        left: 5%;
+    }
+
+    .loading .l-bar {
+        width: 0%;
+        background: #03aac9;
+        left: 0;
+        top: 0;
+        -webkit-transition: width .5s 0;
+    }
+
+    .loading .l-bar span {
+        background: url(/game/static/runner/load.png) no-repeat right center;
+        width: 25px;
+        height: 18px;
+        display: block;
+        right: -8px;
+        top: -8px;
+        position: absolute;
+    }
+
+    .loading .l-txt {
+        position: absolute;
+        text-align: center;
+        top: 40%;
+        width: 100%;
+        font-size: 1rem;
+        color: #fff;
+        text-shadow: 0 3px 1px #000;
+        letter-spacing: .1rem;
+    }
+
+    .loading .l-txt .l-num {
+        color: #fcff00;
+    }
+    #myCanvas{
+        vertical-align: top;
+    }
+    #loading{
+        position: absolute;
+        z-index: 999999;
+        left: 50%;
+        margin-left: -33px;
+        top: 50%;
+        margin-top: -60px;
+    }
+    .game .title{
+        position: absolute;
+        width: 320px;
+        height: 50px;
+        bottom: 180px;
+        left: 50%;
+        margin-left: -160px;
+        font-family: "黑体";
+    }
+    .game .title .time #time{
+        line-height: 70px;
+        text-align: center;
+        color: #e3324c;
+        text-shadow:  2px 0px 0px #fff,-2px 0px 0px #fff,0px 2px 0px #fff,0px -2px 0px #fff;
+        letter-spacing: 2px;
+        font-size: 60px;
+        font-family: arial, "宋体";
+    }
+    .game .title .time{
+        width: 320px;
+        display: none;
+        position: fixed;
+        left: 50%;
+        margin-left: -160px;
+    }
+    .game .title .move{
+        -webkit-transition: -webkit-transform .3s;
+        -webkit-transform:translate3D(-200%,0,0);
+    }
+    .game .title .ready123{
+        margin: auto;
+        text-align: center;
+        display: none;
+    }
+    .game .title .go123{
+        background-image: url("/game/static/runner/go123.png"); display: inline-block;
+        zoom:.8;
+    }
+    .go_3 {background-position: -0px -0px; width: 65px; height: 115px;  }
+    .go_2 {background-position: -66px -0px;
+        width: 94px;
+        height: 115px; }
+    .go_1 {background-position: -161px -0px;
+        width: 94px;
+        height: 115px;}
+    .go_4 {  background-position: -251px -0px;
+        width: 279px;
+        height: 115px;}
+
+    .game .title .result{
+        background: url("/game/static/runner/result.png")no-repeat;
+        background-size: 100% 100%;
+        padding: 12px 30px 0;
+        position: fixed;
+        width: 117.5px;
+        height: 54px;
+        font-size: 14px;
+        left: 50%;
+        margin-left: -93px;
+        top: 134px;
+        color: #fff;
+        font-family: arial, "宋体";
+        -webkit-transition:-webkit-transform .3s;
+        -webkit-animation-delay: .3s;
+        -webkit-transform: translate3D(300%,0,0);
+    }
+    .game .title .result.moveL{
+        -webkit-transform: translate3D(0,0,0);
+    }
+    .game .title .time .metres{
+        color: #e3324c;
+        text-shadow: 1px 0px 0px #fff,-1px 0px 0px #fff,0px 1px 0px #fff,0px -1px 0px #fff;
+        margin-top: 18px;
+        text-align: center;
+        letter-spacing: 2px;
+    }
+    .game .title .time .metres span{
+
+    }
+    .game .title .ready{
+        background: url("/game/static/runner/icon-ready.png") no-repeat;
+        background-size: 100% auto;
+        width: 200px;
+        height: 145px;
+        margin: auto;
+        display: none;
+    }
+
+    .game .title .time .metres{
+        font-size: 20px;
+        margin-top: 10px;
+        font-weight: 600;
+    }
+    .game .title .time #time{
+        font-size: 70px;
+    }
 </style>
 <script>
     export default {
         data() {
-            return {}
+            return {
+                showFingerL: false,
+                showFingerR: false,
+                clickCount : 0,
+                runner     : {}
+            }
         },
-        methods   : {},
+        methods: {
+            touchHandle() {
+                if (this.clickCount === 5) {
+                    if (this.runner.animationV < this.runner.maxV && this.runner.isPlay) {
+                        this.runner.animationV++
+                    }
+                    if (this.runner.runV < 30 && this.runner.isPlay) {
+                        this.runner.runV++
+                    }
+                    this.clickCount = 0
+                }
+            }
+        },
         components: {},
     }
 </script>
